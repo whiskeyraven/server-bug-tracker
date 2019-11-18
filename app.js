4de012504c4dd3fc937a8520aa42d0e27738ce80
freefaller69 <freefaller@gmail.com>
2019-11-17 18:54:44 -0800

    add bugs route

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.use("/api/bugs", bugRoutes);
+app.use("/api/bugs", bugRoutes);
 // app.use("/api/user", userRoutes);
 // app.use("/api/upload", fileRoutes);
 
---
+++ routes/bugs.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// const Bug = require('../models/bug');
+// const checkAuth = require('../middleware/check-auth');
+
+module.exports = router;
