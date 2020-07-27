7c59194e050df6f9ad853b44abb5d80e550d14f2
freefaller69 <freefaller@gmail.com>
2020-07-27 11:13:14 -0700

    update package.json and gitignore files

--- config/db.js
+++
@@ -1,5 +0,0 @@
-const db = {
-  mLAB_DB: `mongodb://bugsAdmin:bananarama82@ds121105.mlab.com:21105/bugtracker`
-}
-
-module.exports = db;
--- config/jwt.js
+++
@@ -1,5 +0,0 @@
-const token = {
-  SECRET: `8&YNIOUhyn*(&yn*uh*7y(*UHN*&Y89Yn8h8&Y87uh&%^D675FuHJOl))`
-}
-
-module.exports = token;
