025d3db21301ea72f51c44c7d8faacad0872f90f
freefaller69 <freefaller@gmail.com>
2020-10-08 14:43:23 -0700

    add express-validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,32 @@
 const express = require('express');
+
 const router = express.Router();
 const { body } = require('express-validator/check');
-const authController = require('../controllers/auth');
+const authController = require('../controllers/authController');
 const User = require('../models/user');
 
 // Create new user
-router.post('/signup', [
-  body('email')
-    .isEmail()
-    .normalizeEmail()
-    .withMessage('Please enter a valid email.')
-    .custom((value, { req }) => {
-      return User.findOne({ email: value })
-        .then(userDoc => {
+router.post(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .normalizeEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
           if (userDoc) {
-            return Promise.reject('Please use a different email address');
+            return Promise.reject(
+              new Error('Please use a different email address')
+            );
           }
         });
-    }),
-  body('password').trim().isLength({ min: 5 }),
-  body('name').trim().isLength({ min: 2 }).not().isEmpty()
-],
-authController.signup);
+      }),
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim().isLength({ min: 2 }).not().isEmpty(),
+  ],
+  authController.signup
+);
 
 router.post('/login', authController.login);
 
